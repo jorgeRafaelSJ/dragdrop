f79ea590d1a45626dcaa692da9c143ff50036bba
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2017-03-12 10:05:31 -0700

    initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		watch: {
+			build: {
+				tasks: ["babel"],
+				files: [
+					'dev.js',
+				]
+			}
+		},
+		babel: {
+	    options: {
+	        sourceMap: true,
+	        presets: ['babel-preset-es2015'],
+	    },
+	    dist: {
+	        files: {
+	            'main.js': 'dev.js'
+	        }
+	    }
+	  }
+	});
+
+	grunt.loadNpmTasks('grunt-babel');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
