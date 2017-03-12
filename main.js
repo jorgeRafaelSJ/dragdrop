bbcdd078e82ad1c5d2c9c7e8d90ef645f411ce93
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2017-03-12 11:52:34 -0700

    laying images in grid and firing drag drop dom events

--- main.js
+++ main.js
@@ -1,28 +1,18 @@
-/*=======================================================================
-
-========================================================================*/
-
-const imageLinks = [
-	'./images/1.png',
-	'./images/2.png',
-	'./images/3.png',
-	'./images/4.png',
-	'./images/5.png',
-	'./images/6.png',
-	'./images/7.png',
-	'./images/8.png',
-	'./images/9.png',
-	'./images/10.png',
-	'./images/11.png', 
-	'./images/12.png'];
+'use strict';
 
 /*=======================================================================
 
 ========================================================================*/
 
-const placeImages = (links) => {
-	links.forEach((link, index) => {
-		let element = document.getElementById(`image-${index + 1}`);
+var imageLinks = ['./images/1.png', './images/2.png', './images/3.png', './images/4.png', './images/5.png', './images/6.png', './images/7.png', './images/8.png', './images/9.png', './images/10.png', './images/11.png', './images/12.png'];
+
+/*=======================================================================
+
+========================================================================*/
+
+var placeImages = function placeImages(links) {
+	links.forEach(function (link, index) {
+		var element = document.getElementById('image-' + (index + 1));
 		element.src = link;
 	});
 };
@@ -33,7 +23,7 @@ placeImages(imageLinks);
 
 ========================================================================*/
 
-const dragImg = (event) => {
+var dragImg = function dragImg(event) {
 	console.log('hello', event);
 };
 
@@ -41,7 +31,7 @@ const dragImg = (event) => {
 
 ========================================================================*/
 
-const dropImg = (event) => {
+var dropImg = function dropImg(event) {
 	event.preventDefault();
 	console.log('goodbye', event);
 };
@@ -50,7 +40,7 @@ const dropImg = (event) => {
 
 ========================================================================*/
 
-const dragOver = (event) => {
+var dragOver = function dragOver(event) {
 	console.log('ondragover');
 	event.preventDefault();
 };
@@ -59,7 +49,8 @@ const dragOver = (event) => {
 
 ========================================================================*/
 
-const dragEnter = (event) => {
+var dragEnter = function dragEnter(event) {
 	console.log('ondragenter');
 	event.preventDefault();
 };
+//# sourceMappingURL=main.js.map
